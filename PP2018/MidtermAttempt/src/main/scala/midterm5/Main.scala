d89980e28211fc26255bce9008e683691a64ba73
Eundo Lee <cobed95@gmail.com>
2018-11-03 19:35:25 +0900

    finished 5-2 of midterm. attempting to solve 5-3.

--- PP2018/MidtermAttempt/src/main/scala/midterm5/Main.scala
+++ PP2018/MidtermAttempt/src/main/scala/midterm5/Main.scala
@@ -8,16 +8,72 @@ object Main {
   case class VarBinLeaf(value: EInt) extends VarBinNode
 
   def parser(l: List[Token]): Exp = {
-    def getEOp(first: Exp, second: TkBop, third: Exp): Exp =
-      EOp(second.o, first, third)
-
-    def slice(l: List[Any], sliced: List[Any]): List[Any] = {
-      l match {
-        case (TkLPar() :: tail) =>
-          slice(tail, sliced
-
+    def parse(stack: List[Exp], queue: List[Token]): Exp = {
+      queue match {
+        case Nil => stack.head
+        case head :: tail =>
+          head match {
+            case TkBop(o) =>
+              stack match {
+                case Nil => EError()
+                case _ :: Nil => EError()
+                case first :: second :: bottom =>
+                  parse(EOp(o, first, second) :: bottom, tail)
+              }
+            case TkInt(n) =>
+              parse(EInt(n) :: stack, tail)
+            case _ => EError()
+          }
       }
     }
+
+    def getPostFix(l: List[Token], stack: List[Token], queue: List[Token]): List[Token] = {
+      println("stack: " + stack)
+      println("queue: " + queue)
+      l match {
+        case Nil =>
+          stack match {
+            case Nil => queue
+            case top :: bottom =>
+              getPostFix(l, bottom, append(queue, top))
+          }
+        case head :: tail =>
+          head match {
+            case TkBop(o) =>
+              stack match {
+                case Nil => getPostFix(tail, head :: stack, queue)
+                case top :: bottom =>
+                  top match {
+                    case TkLPar() => getPostFix(tail, head :: stack, queue)
+                    case _ => getPostFix(l, bottom, append(queue, top))
+                  }
+              }
+            case TkLPar() =>
+              getPostFix(tail, head :: stack, queue)
+            case TkRPar() =>
+              stack match {
+                case Nil => getPostFix(tail, stack, queue)
+                case top :: bottom =>
+                  top match {
+                    case TkLPar() => getPostFix(tail, bottom, queue)
+                    case _ => getPostFix(l, bottom, append(queue, top))
+                  }
+              }
+            case _ => getPostFix(tail, stack, append(queue, head))
+          }
+      }
+    }
+
+    def append(l: List[Token], e: Token): List[Token] =
+      l match {
+        case Nil => List(e)
+        case head :: tail =>
+          head :: append(tail, e)
+      }
+
+    val postfix = getPostFix(l, Nil, Nil)
+    println(postfix)
+    parse(Nil, postfix)
   }
 
 }
--- PP2018/MidtermAttempt/src/main/scala/midterm5/Test.scala
+++ PP2018/MidtermAttempt/src/main/scala/midterm5/Test.scala
@@ -9,5 +9,7 @@ object Test extends App {
 
   println("parser(List(TkInt(4), TkBop(OpSub()), TkInt(8), TkBop(OpMul()), TkInt(9))) is equal to " + parser(List(TkInt(4), TkBop(OpSub()), TkInt(8), TkBop(OpMul()), TkInt(9))))
   println("parser(List(TkInt(8), TkBop(OpAdd()), TkInt(9), TkBop(OpMul()), TkLPar(), TkInt(8), TkBop(OpAdd()), TkInt(4), TkRPar(), TkBop(OpMul()), TkInt(3), TkBop(OpSub()), TkInt(9), TkBop(OpMul()), TkInt(7))) is equal to " + parser(List(TkInt(8), TkBop(OpAdd()), TkInt(9), TkBop(OpMul()), TkLPar(), TkInt(8), TkBop(OpAdd()), TkInt(4), TkRPar(), TkBop(OpMul()), TkInt(3), TkBop(OpSub()), TkInt(9), TkBop(OpMul()), TkInt(7))))
+  // 8 + 9 * (8 + 4) * 3 - 9 * 7
+  // 7 * (9 - (3 * (4 + 8) * (9 + 8)))
   println("parser(List(TkLPar(), TkInt(5), TkBop(OpMul()), TkInt(9))) is equal to " + parser(List(TkLPar(), TkInt(5), TkBop(OpMul()), TkInt(9))))
 }
