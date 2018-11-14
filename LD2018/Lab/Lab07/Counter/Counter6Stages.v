041e5a0e50ce536b9ce2b382394aabd5eb518799
Eundo Lee <cobed95@gmail.com>
2018-11-14 15:22:03 +0900

    added Counter.

---
+++ LD2018/Lab/Lab07/Counter/Counter6Stages.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:11:36 11/14/2018 
+// Design Name: 
+// Module Name:    Counter6Stages 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Counter6Stages(
+    input clk,
+    output [2:0] out
+    );
+
+
+endmodule
