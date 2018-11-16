bb7003dc513142487995ce1ee9bfcd654af53a01
Eundo Lee <cobed95@gmail.com>
2018-11-16 16:59:44 +0900

    finished lab11.

--- DS2018/Lab/Lab11/lab_11_QBS/src/dS/QBS.java
+++ DS2018/Lab/Lab11/lab_11_QBS/src/dS/QBS.java
@@ -4,12 +4,96 @@ public class QBS<E extends Comparable<? super E>> {
 	public QBS() {}
 	public int qbs(E[] arr, E e, int len) {
 		// fill your code
-		
-
+        if (len <= 10)
+            return binarySearch(arr, e, 0, len - 1);
+        else {
+            double p = getP(arr, e, len);
+            int pn = getPN(p, len);
+            int sqrt = sqrt(1.0, len);
+            if (e.compareTo(arr[pn]) == 0) return pn;
+            else if (e.compareTo(arr[pn]) < 0)
+                return probeDown(arr, e, len, pn, sqrt);
+            else return probeUp(arr, e, len, pn, sqrt);
+        }
 
 	}
-	
+
+	public double getP(E[] arr, E target, int len) {
+	    if (target instanceof Integer) {
+            Integer targetInt = (Integer) target;
+            Integer arr1 = (Integer) arr[0];
+            Integer arrN = (Integer) arr[len - 1];
+            if (len == 0 || len == 1) return 0;
+            return (double) (targetInt - arr1) / (arrN - arr1);
+        }
+        return -1;
+    }
+
+    public int getPN(double p, int len) {
+	    return (int) (p * len);
+    }
+
+    public int probeDown(E[] arr, E target, int len, int curr, int jump) {
+	    if (curr <= 0) return 0;
+        if (target.compareTo(arr[curr]) >= 0)
+            return linearSearch(arr, target, curr, curr + jump, false);
+        else return probeDown(arr, target, len, curr - jump, jump);
+    }
+
+    public int probeUp(E[] arr, E target, int len, int curr, int jump) {
+	    if (curr >= len) return len - 1;
+	    if (target.compareTo(arr[curr]) <= 0)
+	        return linearSearch(arr, target, curr, curr - jump, true);
+	    else return probeUp(arr, target, len, curr + jump, jump);
+    }
+
+    public int linearSearch(E[] arr, E target, int from, int to, boolean reverse) {
+	    if (reverse) {
+	        for (int i = from; i > to; i--) {
+                if (target.compareTo(arr[i]) == 0)
+                    return i;
+                else if (target.compareTo(arr[i]) > 0)
+                    return i + 1;
+            }
+            return to + 1;
+        } else {
+            for (int i = from; i < to; i++) {
+                if (target.compareTo(arr[i]) == 0)
+                    return i;
+                else if (target.compareTo(arr[i]) < 0)
+                    return i + 1;
+            }
+            return to;
+        }
+    }
 	// Maybe you need to create the helper methods.
-	
+    public int sqrt(double guess, int n) {
+        if (isGoodEnough(guess, n)) return (int) guess;
+        else return sqrt(improve(guess, n), n);
+    }
+
+    public boolean isGoodEnough(double guess, int n) {
+	    double diff = (guess * guess) - n;
+	    return diff <= 0.1 && diff >= -0.1;
+    }
+
+    public double improve(double guess, int n) {
+        return (guess + n / guess) / 2;
+    }
+
+    public int binarySearch(E[] arr, E target, int from, int to) {
+        if (to - from == 1) {
+            if (target.compareTo(arr[from]) == 0) return from;
+            else if (target.compareTo(arr[from]) < 0) return from;
+            else return from + 1;
+        } else {
+            int mid = (to + from) / 2;
+            if (target.compareTo(arr[mid]) == 0) return mid;
+            else if (target.compareTo(arr[mid]) < 0)
+                return binarySearch(arr, target, from, mid);
+            else
+                return binarySearch(arr, target, mid, to);
+        }
+    }
 
 }
--- DS2018/Lab/Lab11/lab_11_QBS/src/dS/Sort.java
+++ DS2018/Lab/Lab11/lab_11_QBS/src/dS/Sort.java
@@ -17,31 +17,56 @@ public class Sort<E extends Comparable<? super E>>{
 	/** Initialize Array */
 	public void clear() {
 		// fill your code
-
-	 
+		arr = newArray(100);
+		len = 0;
 	}
  
 	public void insert(E e) {
 		// fill your code
-		
-
-	} 
+        if (len == 0) {
+            arr[0] = e;
+            len++;
+        } else if (e.compareTo(arr[0]) < 0) {
+            for (int i = len; i >= 0; i--)
+                arr[i + 1] = arr[i];
+            arr[0] = e;
+            len++;
+        } else if (e.compareTo(arr[len - 1]) > 0) {
+            arr[len] = e;
+            len++;
+        } else {
+            int idx = search.qbs(arr, e, len);
+            if (e.compareTo(arr[idx]) != 0 && len <= arr.length) {
+                for (int i = len; i >= idx; i--)
+                    arr[i + 1] = arr[i];
+                arr[idx] = e;
+                len++;
+            }
+        }
+	}
 	
 	public int remove(E e) {
 		// fill your code
-		
-
+		int idx = search.qbs(arr, e, len);
+		if (e.compareTo(arr[idx]) == 0) {
+		    for (int i = idx; i < len - 1; i++)
+		        arr[i] = arr[i + 1];
+		    len--;
+		    return idx;
+        } else return -1;
 	}
 	
 	public int find(E e) {
 		//fill your code
-	
-
+		int idx = search.qbs(arr, e, len);
+		if (e.compareTo(arr[idx]) != 0)
+		    return -1;
+		else return idx;
 	}
 	
 	public int size() {
 		// fill your code
-		
+		return len;
 	}
 	
 	public void print() {
