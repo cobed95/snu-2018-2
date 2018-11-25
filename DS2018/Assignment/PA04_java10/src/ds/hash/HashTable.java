4333ddecff603d40be81c57f31f488a4ef19e7cd
Eundo Lee <cobed95@gmail.com>
2018-11-25 16:45:46 +0900

    finished PA04.

--- DS2018/Assignment/PA04_java10/src/ds/hash/HashTable.java
+++ DS2018/Assignment/PA04_java10/src/ds/hash/HashTable.java
@@ -6,17 +6,28 @@ public class HashTable{
 	int c1, c2, c3;
 	
 	public HashTable(int n) {
-		// fill in your code 
+		// fill in your code
+        table = new Item[n];
 	}
 
 	public void Create(int c1, int c2, int c3){
-		// fill in your code 
+		// fill in your code
+		if (c1 == 0 && c2 == 0 && c3 == 0) return;
+	    if (c1 < 0 || c2 < 0 || c3 < 0) return;
+	    this.c1 = c1;
+	    this.c2 = c2;
+	    this.c3 = c3;
 	}
 	
 	public void Insert(String key, int value){
 		// fill in your code 
-		
-		int index = 0;
+	    // This code cannot insert into "deleted" slots.
+		int index = probeQuadratically(key, false);
+	    if (table[index] == null) {
+	        index = probeQuadratically(key, true);
+	        table[index] = new Item(key, value);
+        }
+	    else table[index].insert(value);
 		
 		/** This is the sample output format. 
 		 *  You'd better use this format to output your answer.
@@ -27,26 +38,61 @@ public class HashTable{
 	public void Delete(String key){
 		// fill in your code 
 		
-		int value = 0;
-		int index = 0;
-		
+		int index = probeQuadratically(key, false);
+		if (table[index] == null)
+            System.out.println("Failed to find " + key);
+		else {
+		    int value = table[index].popValue();
+            System.out.println("Delete: (" + key + ", " + value + ") / " + "INDEX: " + index);
+        }
+
 		/** This is the sample output format. 
 		 *  You'd better use this format to output your answer.
 		 */
-		System.out.println("Delete: (" + key + ", " + value + ") / " + "INDEX: " + index);
-		System.out.println("Failed to find " + key);
 	}
 	
 	public void Search(String key){
 		// fill in your code 
 		
-		int value = 0;
-		int index = 0;
-		
+		int index = probeQuadratically(key, false);
+		if (table[index] == null)
+            System.out.println("Failed to find " + key);
+		else {
+		    int value = table[index].getValue();
+            System.out.println("Search: (" + key + ", " + value + ") / " + "INDEX: " + index);
+        }
+
 		/** This is the sample output format. 
 		 *  You'd better use this format to output your answer.
 		 */
-		System.out.println("Search: (" + key + ", " + value + ") / " + "INDEX: " + index);
-		System.out.println("Failed to find " + key);
 	}
+
+	public int hash(String key) {
+	    int hashCode = key.hashCode();
+	    int M = table.length;
+	    return hashCode % M;
+    }
+
+    public int getIdxQuadratically(int hashCode, int i) {
+	    int jump = (c1 * i * i) + (c2 * i) + c3;
+	    return hashCode + jump;
+    }
+
+    public int probeQuadratically(String key, boolean searchForDeleted) {
+	    int M = table.length;
+	    int hashCode = hash(key);
+	    int idx = hashCode;
+	    int i = 1;
+	    while (notAtTerminal(searchForDeleted, idx) && !table[idx].getKey().equals(key)) {
+	        idx = getIdxQuadratically(hashCode, i) % M;
+	        i++;
+        }
+        return idx;
+    }
+
+    public boolean notAtTerminal(boolean searchForDeleted, int idx) {
+	    if (searchForDeleted)
+	        return table[idx] != null && !table[idx].getKey().equals("deleted");
+	    else return table[idx] != null;
+    }
 }
--- DS2018/Assignment/PA04_java10/src/ds/hash/Item.java
+++ DS2018/Assignment/PA04_java10/src/ds/hash/Item.java
@@ -8,6 +8,37 @@ public class Item {
 	private String key;
 	private MaxHeap<Integer> maxHeap;
 	
-	// fill in your code 
-	
+	// fill in your code
+	public Item(String key) {
+		this.key = key;
+		Integer[] array = new Integer[5];
+		this.maxHeap = new MaxHeap<Integer>(array, 0, 5);
+	}
+
+	public Item(String key, Integer value) {
+		this(key);
+		this.insert(value);
+	}
+
+	public void insert(Integer value) {
+		maxHeap.insert(value);
+	}
+
+	public String getKey() {
+	    return key;
+    }
+
+    public Integer popValue() {
+	    Integer value = maxHeap.removemax();
+	    if (maxHeap.heapsize() == 0) key = "deleted";
+	    return value;
+    }
+
+    public Integer getValue() {
+	    return maxHeap.peek();
+    }
+
+    public int getSize() {
+	    return maxHeap.heapsize();
+    }
 }
